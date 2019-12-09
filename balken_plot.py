8b7267b43205af1aaf212bf4cce2d2bbbec09fc1
Bluboska <49392055+Bluboska@users.noreply.github.com>
2019-12-09 21:31:12 +0100

    testplot

--- balken_plot.py
+++ balken_plot.py
@@ -2,3 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+objects = ('Target', 'Target', 'Target', 'Target', 'Target', 'Target', 'Target')
+y_pos = np.arange(len(objects))
+performance = [10,8,6,4,2,1,0]
+
+plt.barh(y_pos, performance, align='center', alpha=0.5)
+plt.yticks(y_pos, objects)
+plt.xlabel('Usage')
+plt.title('Programming language usage')
+
+plt.show()
